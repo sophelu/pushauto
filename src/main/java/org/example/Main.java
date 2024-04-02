90f4a82f998ece890ceca7843b6e8a70d05d0d94
lusophie <171341531@qq.com>
2024-04-02 16:21:10 +0800

    push

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("data--");
         System.out.println("Hello world!");
+        System.out.println("push");
     }
 }
